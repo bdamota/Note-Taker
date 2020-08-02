8777bb0a0b2a70b5b818037bab50b1233b0885c9
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-01 22:05:19 -0700

    unique id added for each note

--- apiRoutes/notesRoutes.js
+++ apiRoutes/notesRoutes.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
 const { createNewNote } = require("../lib/notes");
 const { notes } = require("../data/db");
-// const uniqueId = require("uniqueId")
+const uniqid = require("uniqid")
 
 router.get("/notes", (req, res) => {
     let results = notes
     res.json(results)
 })
 
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
 router.post("/notes", (req, res) => {
-    //set id randomly
-    // req.body.id = uniqueId()
+    //set id 
+    req.body.id = uniqid()
     const note = createNewNote(req.body, notes)
     res.json(note);
 })
