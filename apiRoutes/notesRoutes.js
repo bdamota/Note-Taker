b3dcc694ae13a1b4e6a71953d8b3d323d47b97fd
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-01 22:53:18 -0700

    Add Heroku

--- apiRoutes/notesRoutes.js
+++ apiRoutes/notesRoutes.js
@@ -8,14 +8,6 @@ router.get("/notes", (req, res) => {
     res.json(results)
 })
 
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-  });
 
 router.post("/notes", (req, res) => {
     //set id 
